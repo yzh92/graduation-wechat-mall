220be27fb7311ab19ba7974e20ba947f880f52c1
yzh <2426555967@qq.com>
2021-04-22 08:34:04 +0800

    地址接口

---
+++ shop-service/src/main/java/com/shop/dao/UserAddrMapper.java
@@ -0,0 +1,25 @@
+package com.shop.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.shop.bean.model.UserAddr;
+import org.apache.ibatis.annotations.Param;
+
+public interface UserAddrMapper extends BaseMapper<UserAddr> {
+
+	UserAddr getDefaultUserAddr(@Param("userId") String userId);
+
+	/**
+	 * 移除用户默认地址
+	 * @param userId
+	 */
+	void removeDefaultUserAddr(@Param("userId") String userId);
+
+	/**
+	 * 将地址设置为默认地址
+	 * @param addrId
+	 * @param userId
+	 */
+	int setDefaultUserAddr(@Param("addrId") Long addrId, @Param("userId") String userId);
+
+	UserAddr getUserAddrByUserIdAndAddrId(@Param("userId") String userId, @Param("addrId") Long addrId);
+}
---
+++ shop-service/src/main/java/com/shop/service/UserAddrService.java
@@ -0,0 +1,22 @@
+package com.shop.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.shop.bean.model.UserAddr;
+
+public interface UserAddrService extends IService<UserAddr> {
+
+	UserAddr getDefaultUserAddr(String userId);
+
+	/**
+	 * 更新默认地址
+	 * @param addrId 默认地址id
+	 * @param userId 用户id
+	 */
+	void updateDefaultUserAddr(Long addrId, String userId);
+
+
+    void removeUserAddrByUserId(Long addrId, String userId);
+
+    UserAddr getUserAddrByUserId(Long addrId, String userId);
+}
+
