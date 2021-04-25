288568263b138803f97a716f8b4c4153fe730fc4
yzh <2426555967@qq.com>
2021-04-26 00:59:43 +0800

    后台：订单管理

--- shop-bean/src/main/java/com/shop/bean/param/OrderParam.java
+++ shop-bean/src/main/java/com/shop/bean/param/OrderParam.java
@@ -1,34 +1,42 @@
 package com.shop.bean.param;
 
-import com.shop.bean.app.param.OrderItemParam;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.NotNull;
-import java.util.List;
+import java.util.Date;
 
-/**
- * @author yzh
- */
 @Data
-@ApiModel(value= "订单参数")
 public class OrderParam {
+    /**
+     * 店铺id
+     */
+    private Long shopId;
 
+    /**
+     * 订单状态 -1 已取消 0:待付款 1:待发货 2:待收货 3:已完成
+     */
+    private Integer status;
 
-	@ApiModelProperty(value = "购物车id 数组")
-	private List<Long> basketIds;
+    /**
+     * 是否已经支付，1：已经支付过，0：，没有支付过
+     */
+    private Integer isPayed;
 
-	@ApiModelProperty(value = "立即购买时提交的商品项")
-	private OrderItemParam orderItem;
+    /**
+     * 订购流水号
+     */
+    private String orderNumber;
 
-	@ApiModelProperty(value = "地址ID，0为默认地址",required=true)
-	@NotNull(message = "地址不能为空")
-	private Long addrId;
+    /**
+     * 开始时间
+     */
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date startTime;
 
-	@ApiModelProperty(value = "用户是否改变了优惠券的选择，如果用户改变了优惠券的选择，则完全根据传入参数进行优惠券的选择")
-	private Integer userChangeCoupon;
+    /**
+     * 结束时间
+     */
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date endTime;
 
-	@ApiModelProperty(value = "优惠券id数组")
-	private List<Long> couponIds;
 }
